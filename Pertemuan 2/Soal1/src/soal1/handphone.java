3c5d87a29bebcb1a1e100e45e6075ea51244d047
ɢᴀɴɪsᴀᴛʀɪᴀ <ganisatria5@gmail.com>
2022-03-19 09:50:25 +0700

    commit
    Pertemuan 2 PBO

---
+++ Pertemuan 2/Soal1/src/soal1/handphone.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package soal1;
+
+/**
+ *
+ * @author ganisatria
+ */
+public class handphone {
+    
+
+Sting On_Handphone;
+Call_Phone
+Send_sms
+Off_Handphone
+
+    
+}
