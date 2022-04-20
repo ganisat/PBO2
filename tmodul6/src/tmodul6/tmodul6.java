c4549d184b9871be664012b6233a648ffd3ba78d
ɢᴀɴɪsᴀᴛʀɪᴀ <ganisatria5@gmail.com>
2022-04-21 04:08:19 +0700

    prak 1
    prak 1

---
+++ tmodul6/src/tmodul6/tmodul6.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tmodul6;
+import java.text.*;
+/**
+ *
+ * @author Gani
+ */
+public class tmodul6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new p6().setVisible(true);
+    }
+    
+}
